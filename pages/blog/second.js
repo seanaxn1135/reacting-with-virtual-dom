7d970e6b4c7f2faef7d91b2ac608570eaa62d85e
seanaxn1135 <seanaxn1135@gmail.com>
2023-06-10 14:07:09 +0800

    first commit

---
+++ pages/blog/second.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function second() {
+  return (
+    <div>
+      <h1>Second Post</h1>
+    </div>
+  );
+}
---
+++ pages/firstpage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function firstpage() {
+  return (
+    <div>
+      <h1>First page</h1>
+    </div>
+  );
+}
